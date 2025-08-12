c2f3c450fb63fbf3d046947027a7c4180bfa21ba
Mart2265 <mart_2265@hotmail.com>
2025-08-12 11:46:57 +0200

    Delete app directory

--- app/api/quote/route.ts
+++
@@ -1,8 +0,0 @@
-
-import { NextResponse } from 'next/server'
-
-export async function POST(request: Request) {
-  const data = await request.json()
-  console.log('Ontvangen offerte:', data)
-  return NextResponse.json({ message: 'Offerte ontvangen' })
-}
